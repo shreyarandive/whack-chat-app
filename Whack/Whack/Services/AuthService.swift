1f3739712eab4e89f5124ecf723c91408073e89b
Shreya Randive <shreya@pal-nat186-122-92.itap.purdue.edu>
2019-06-11 19:22:12 -0400

    Use SwiftyJson

--- Whack/Whack/Services/AuthService.swift
+++ Whack/Whack/Services/AuthService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class AuthService {
     
@@ -74,15 +75,31 @@ class AuthService {
         Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
             
             if response.result.error == nil {
-                if let json = response.result.value as? Dictionary<String, Any> {
+                /*if let json = response.result.value as? Dictionary<String, Any> {
                     if let email = json["user"] as? String {
                         self.userEmail = email
                     }
                     if let token = json["token"] as? String {
                         self.authToken = token
                     }
+                }*/
+                //swiftyJSON
+                
+                guard let data = response.data else { return }
+                
+                do {
+                    let json = try JSON(data: data)
+                    
+                    self.userEmail = json["user"].stringValue
+                    self.authToken = json["token"].stringValue
+                    
+                    self.isLoggedin = true
+                    completion(true)
+                    
+                } catch let error as NSError {
+                    print(error)
                 }
-                completion(true)
+                
             } else {
                 completion(false)
                 debugPrint(response.result.error as Any)
