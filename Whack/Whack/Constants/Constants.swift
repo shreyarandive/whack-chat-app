2c02bb7935a5ea9422ed0cafbf024da4a00b8d97
Shreya Randive <shreya@pal-nat186-122-92.itap.purdue.edu>
2019-06-10 20:13:05 -0400

    Add register user func

--- Whack/Whack/Constants/Constants.swift
+++ Whack/Whack/Constants/Constants.swift
@@ -8,6 +8,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success : Bool) -> ()
+
+//segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND =  "unwindToChannel"
+
+//user defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+//urls
+let BASE_URL = "https://whackchat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+ 
---
+++ Whack/Whack/Services/AuthService.swift
@@ -0,0 +1,67 @@
+//
+//  AuthService.swift
+//  Whack
+//
+//  Created by Shreya Randive on 6/10/19.
+//  Copyright © 2019 Shreya Randive. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    var isLoggedin : Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken: String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail: String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
+        let lowercaseEmail = email.lowercased()
+        let header = [
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowercaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            
+            if response.result.error == nil {
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+}
